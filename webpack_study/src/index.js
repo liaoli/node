cc24ee0a01e06fb945a8e71e7b7a8317e394fb37
liaoli <277218962@qq.com>
2023-09-08 18:29:56 +0800

    webpack 打包

---
+++ webpack_study/src/index.js
@@ -0,0 +1,5 @@
+import { checkPhone, checkCode } from "./utils/check.js";
+console.log(checkPhone(17722536489))
+console.log(checkPhone(1772253648))
+console.log(checkCode(1772253648))
+console.log(checkCode(123456))
